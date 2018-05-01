ea9c5fab9656e2447abd4926638e107821801082
bellinda <gabriela.angelova@fdiba.tu-sofia.bg>
2018-05-01 10:39:37 +0300

    initial commit for the project
    Signed-off-by: bellinda <gabriela.angelova@fdiba.tu-sofia.bg>

---
+++ app/src/main/java/com/angelova/w510/calmmom/LoginActivity.java
@@ -0,0 +1,128 @@
+package com.angelova.w510.calmmom;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.angelova.w510.calmmom.dialogs.WarnDialog;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.melnykov.fab.FloatingActionButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private FloatingActionButton mFloatingBtn;
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    System.out.println("onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    System.out.println("onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        mFloatingBtn = (FloatingActionButton) findViewById(R.id.add_new_user_btn);
+
+        mFloatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+
+        mEmail = (EditText) findViewById(R.id.input_username);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEmail.getText() != null && !mEmail.getText().toString().isEmpty()) {
+                    if (mPassword.getText() != null && !mPassword.getText().toString().isEmpty()) {
+                        loginUser(mEmail.getText().toString(), mPassword.getText().toString());
+                    } else {
+                        showAlertDialogNow("Please input your password", "Login");
+                    }
+                } else {
+                    showAlertDialogNow("Please input your email", "Login");
+                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        System.out.println("signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            System.out.println("signInWithEmail:failed" + task.getException());
+                            Toast.makeText(LoginActivity.this, "Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void showAlertDialogNow(String message, String title) {
+        WarnDialog warning = new WarnDialog(this, title, message, new WarnDialog.DialogClickListener() {
+            @Override
+            public void onClick() {
+            }
+        });
+        warning.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
---
+++ app/src/main/java/com/angelova/w510/calmmom/RegisterActivity.java
@@ -0,0 +1,119 @@
+package com.angelova.w510.calmmom;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.angelova.w510.calmmom.dialogs.WarnDialog;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mRepeatPassword;
+    private Button mRegisterBtn;
+    private ProgressBar mLoader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor("#324A5F"));
+        }
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmail = (EditText) findViewById(R.id.input_username);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mRepeatPassword = (EditText) findViewById(R.id.input_password_repeat);
+
+        mRegisterBtn = (Button) findViewById(R.id.register_btn);
+        mLoader = (ProgressBar) findViewById(R.id.register_loader);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRegisterBtn.setVisibility(View.GONE);
+                mLoader.setVisibility(View.VISIBLE);
+                if (mEmail.getText() != null && !mEmail.getText().toString().isEmpty()) {
+                    if (mPassword.getText() != null && !mPassword.getText().toString().isEmpty()) {
+                        if (mRepeatPassword.getText() != null && !mRepeatPassword.getText().toString().isEmpty()) {
+                            if (mPassword.getText().toString().equals(mRepeatPassword.getText().toString())) {
+                                registerUser(mEmail.getText().toString(), mPassword.getText().toString());
+                            } else {
+                                showAlertDialogNow("Both passwords dont match. Please check them again.", "User registration");
+                            }
+                        } else {
+                            showAlertDialogNow("Please repeate your password.", "User registration");
+                        }
+                    } else {
+                        showAlertDialogNow("Please enter a password.", "User registration");
+                    }
+                } else {
+                    showAlertDialogNow("Please enter your email.", "User registration");
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        System.out.println("createUserWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            showAlertDialogNow(task.getException().getMessage(), "User registration");
+                        } else {
+                            showAlertDialogNow("You are registered successfully", "User registration", new WarnDialog.DialogClickListener() {
+                                @Override
+                                public void onClick() {
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                        }
+                        mLoader.setVisibility(View.GONE);
+                        mRegisterBtn.setVisibility(View.VISIBLE);
+                    }
+                });
+    }
+
+    private void showAlertDialogNow(String message, String title) {
+        WarnDialog warning = new WarnDialog(this, title, message, new WarnDialog.DialogClickListener() {
+            @Override
+            public void onClick() {
+                mLoader.setVisibility(View.GONE);
+                mRegisterBtn.setVisibility(View.VISIBLE);
+            }
+        });
+        warning.show();
+    }
+
+    private void showAlertDialogNow(String message, String title, WarnDialog.DialogClickListener listener) {
+        WarnDialog warnDialog = new WarnDialog(this, title, message, listener);
+        warnDialog.show();
+    }
+}
